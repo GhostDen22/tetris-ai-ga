e7c715e52be87663233d2ae5f9b679a58e93b0e2
GhostDen22 <dencikszapowalow@gmail.com>
2026-05-05 19:37:51 +0300

    Add population fitness evaluation

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
-from ga.population import create_initial_population
+from ga.population import create_initial_population, evaluate_population, get_best_genome
 
 
 def main():
     population = create_initial_population(size=5, seed=123)
 
-    print("INITIAL POPULATION")
+    evaluate_population(population, max_moves=100)
 
-    for index, genome in enumerate(population):
-        print(f"\nGenome {index}")
-        print(genome.get_weights())
+    best = get_best_genome(population)
+
+    print("\nBEST GENOME")
+    print("Fitness:", best.get_fitness())
+    print("Weights:", best.get_weights())
 
 
 if __name__ == "__main__":
